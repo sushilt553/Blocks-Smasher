2a64f0ec980c6b395574f28c654889b5d82ed0cc
Sushil <sushilt553@gmail.com>
2020-04-13 19:12:40 -0400

    Wall hit done

--- src/scripts/Game.js
+++ src/scripts/Game.js
@@ -8,22 +8,32 @@ class Game {
         this.ctx = ctx;
         this.x = this.canvas.width / 2;
         this.y = this.canvas.height - 30;
+        this.dx = 2;
+        this.dy = -2;
         this.draw = this.draw.bind(this);
+        this.checkWallHit = this.checkWallHit.bind(this);
         this.interval = setInterval(this.draw, 10);
+    }
+
+    checkWallHit() {
+        if (this.x + this.dx > this.canvas.width || this.x + this.dx < 0) {
+            this.dx = - this.dx;
+        }else if(this.y + this.dy < 0) {
+            this.dy = - this.dy;
+        }
     }
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        const dx = 2;
-        const dy = -2;
 
         var ball = new Ball(this.canvas, this.ctx, this.x, this.y);
         var paddle = new Paddle(this.canvas, this.ctx);
 
         ball.drawBall();
         paddle.drawPaddle();
-        this.x += dx;
-        this.y += dy;
+        this.checkWallHit();
+        this.x += this.dx;
+        this.y += this.dy;
     }
 }
 
