3d53a18365676b7776a8c98eb7a1ce95e5fc066a
Sushil <sushilt553@gmail.com>
2020-04-14 15:18:24 -0400

    Welcome game

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import Game from './scripts/Game';
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
-    const game = new Game(canvas, ctx);
+
+    const play = document.getElementById("home-page");
+    const welcome = document.getElementById("welcome");
+    play.addEventListener("click", () => {
+        play.classList.add("hidden");
+        welcome.classList.add("hidden");
+        new Game(canvas, ctx);
+    })
 })
