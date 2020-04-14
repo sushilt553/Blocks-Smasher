f61aaccb78751b2a705d04f379075c893d375d42
Sushil <sushilt553@gmail.com>
2020-04-13 22:49:37 -0400

    Brick break collision done

--- src/scripts/Game.js
+++ src/scripts/Game.js
@@ -19,6 +19,7 @@ class Game {
         this.brickHeight = 30;
         this.brickPadding = 20; 
         this.sidePadding = 50;
+        this.bricks = [];
 
         this.paddleHeight = 10;
         this.paddleWidth = 75;
@@ -30,10 +31,14 @@ class Game {
         this.draw = this.draw.bind(this);
         this.checkWallHit = this.checkWallHit.bind(this);
 
+        this.drawBricks = this.drawBricks.bind(this);
+        this.detectCollision = this.detectCollision.bind(this);
+
         this.onClick = this.onClick.bind(this);
         this.offClick = this.offClick.bind(this);
+        this.bricksObjectArray();
 
-        this.interval = setInterval(this.draw, 10);
+        this.interval = setInterval(this.draw, 8);
 
         document.addEventListener("keydown", this.onClick);
         document.addEventListener("keyup", this.offClick);
@@ -58,11 +63,13 @@ class Game {
     checkWallHit() {
         if (this.x + this.dx > this.canvas.width - this.ballRadius || this.x + this.dx < this.ballRadius) {
             this.dx = - this.dx;
+            // this.dy -= 1;
         }else if(this.y + this.dy < this.ballRadius) {
             this.dy = - this.dy;
         }else if (this.y + this.dy > this.canvas.height - this.ballRadius) {
             if (this.x > this.paddleX && this.x < this.paddleX + this.paddleWidth) {
                 this.dy = - this.dy;
+                // this.dx += 1;
             }else {
                 alert("Game Over");
                 clearInterval();
@@ -70,22 +77,63 @@ class Game {
         }
     }
 
+    bricksObjectArray() {
+
+        for (let i = 0; i < this.brickColumn; i++) {
+            this.bricks[i] = [];
+            for (let j = 0; j < this.brickRow; j++) {
+                this.bricks[i][j] = {x: 0, y: 0, status: true}
+            }
+        }
+    }
+
+
+    detectCollision() {
+
+        for (let i = 0; i < this.brickColumn; i++) {
+            for (let j = 0; j < this.brickRow; j++) {
+                var brick = this.bricks[i][j];
+                if (brick.status) {
+                    if ((this.x > brick.x && this.x < brick.x + this.brickWidth) && (this.y > brick.y && this.y < brick.y + this.brickHeight)) {
+                        this.dy = - this.dy;
+                        brick.status = false;
+                    }
+                }
+            }
+        }
+    }
+
+    drawBricks() {
+        var singleBrick;
+
+        for (let i = 0; i < this.brickColumn; i++) {
+            for (let j = 0; j < this.brickRow; j++) {
+                var brick = this.bricks[i][j];
+                if (brick.status) {
+                    var brickX = (i * (this.brickWidth + this.brickPadding)) + this.sidePadding;
+                    var brickY = (j * (this.brickHeight + this.brickPadding)) + this.sidePadding;
+
+                    this.bricks[i][j].x = brickX;
+                    this.bricks[i][j].y = brickY;
+
+                    singleBrick = new Brick(this.ctx, brickX, brickY, this.brickWidth, this.brickHeight);
+                    singleBrick.drawBrick();
+                }
+            }
+        }
+    }
+
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawBricks();
 
         var ball = new Ball(this.canvas, this.ctx, this.x, this.y, this.ballRadius);
         var paddle = new Paddle(this.canvas, this.ctx, this.paddleHeight, this.paddleWidth, this.paddleX);
-        var brick;
-        for (let i = 0; i < this.brickColumn; i++) {
-            for (let j = 0; j < this.brickRow; j++){
-                brick = new Brick(this.ctx, (i * (this.brickWidth + this.brickPadding)) + this.sidePadding, (j * (this.brickHeight + this.brickPadding)) + this.sidePadding, this.brickWidth, this.brickHeight);
-                brick.drawBrick();
-            }
-        }
 
         ball.drawBall();
         paddle.drawPaddle();
 
+        this.detectCollision();
         this.checkWallHit();
 
         if (this.rightClick) {
